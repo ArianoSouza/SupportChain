1996cf2c0d54ca8b76016813554c2e70e9d2d0ac
laysa <142953600+laysafreitas@users.noreply.github.com>
2025-05-18 12:56:42 -0300

    ajeitei e adicionei termos de uso e envio de dados na função ccadastro

--- backend/src/data/connection.ts
+++ backend/src/data/connection.ts
@@ -9,7 +9,7 @@ const connection = knex ({
       host:"127.0.0.1",
       port: 3306,
       user: "root" ,
-      password: process.env.DB_PASSWORD,
+      password: "lalay1810",
       database: "SupportChain",
       multipleStatements: true
       }
