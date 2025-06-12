919970cc03af9aa96e74054f2858d65e1421c3e6
Ariano <arianosouzapro@gmail.com>
2025-06-12 19:07:29 -0300

    hotfix 4

--- backend/src/data/connection.ts
+++ backend/src/data/connection.ts
@@ -3,17 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = knex ({
-  client: 'pg', // Alterado para 'pg' para PostgreSQL
-  connection: {
-    host: process.env.DB_HOST , // Usando variáveis de ambiente do Render
-    port:Number(process.env.DB_PORT) , // Usando variáveis de ambiente do Render
-    user: process.env.DB_USER, // Usando variáveis de ambiente do Render
-    password: process.env.DB_PASSWORD, // Usando variáveis de ambiente do Render
-    database: process.env.DB_NAME, // Usando variáveis de ambiente do Render
-  },
+const connection = knex({
+  client: 'pg',
+  // Use a DATABASE_URL fornecida pelo Render e adicione '?sslmode=require'
+  // Certifique-se de que process.env.DATABASE_URL esteja configurada no Render!
+  connection: process.env.DB_URL + '?sslmode=require',
   pool: { min: 2, max: 10 }
-})
+});
 
 export default connection;
 
